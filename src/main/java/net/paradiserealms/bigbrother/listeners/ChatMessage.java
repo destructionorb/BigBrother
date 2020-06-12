757db5284d8688507228ba2a6ef5fa9388fb22d9
Cael Burkhardt <neonepsd@gmail.com>
2020-06-12 19:26:15 -0400

    Add HeroChat compatibility to ChatMessage.java

--- src/main/java/net/paradiserealms/bigbrother/listeners/ChatMessage.java
+++ src/main/java/net/paradiserealms/bigbrother/listeners/ChatMessage.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class ChatMessage implements Listener {
 
@@ -17,6 +18,22 @@ public class ChatMessage implements Listener {
         for(Player online : Bukkit.getOnlinePlayers()) {
             if(online.hasPermission("bigbrother.cm")) {
                 online.sendMessage(ChatColor.GRAY + "[" + player + ": " + message + "]");
+            }
+        }
+    }
+
+    @EventHandler
+    public void onHeroChatMessage(PlayerCommandPreprocessEvent e) {
+        Player player = e.getPlayer();
+        String command = e.getMessage();
+        String commandSplit[] = command.split(" ", 2);
+        command = commandSplit[0];
+
+        if(command.equals("/g") || command.equals("/h") || command.equals("/t") || command.equals("/l")) {
+            for(Player online : Bukkit.getOnlinePlayers()) {
+                if(online.hasPermission("bigbrother.cm")) {
+                    online.sendMessage(ChatColor.GRAY + "[" + player + ": " + e.getMessage() + "]");
+                }
             }
         }
     }
